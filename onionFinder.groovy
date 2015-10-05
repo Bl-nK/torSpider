49ea97b0e1405503705f5313c9f4a650e37008a4
Brandon <brandon@bl-nk.net>
2015-10-05 17:26:04 -0500

    Some cleanup

--- onionFinder.groovy
+++ onionFinder.groovy
@@ -1,25 +1,24 @@
 @Grapes(
 	@Grab(group='org.codehaus.gpars', module='gpars', version='1.2.1')
-)
-
+    )
 
 import static groovyx.gpars.GParsPool.withPool
 
 System.properties.putAll( ["proxySet":"true","socksProxyHost":"localhost", "socksProxyPort":"9050"] )
 def spider(address){
     try {
-
-    String data = new URL(address).getText()
-    def uniqueOnions = data.findAll(/(?:([a-z]+):\/\/){0,1}([a-z2-7]{16})\.onion(?::(\d+)){0,1}/).unique()
-    if(uniqueOnions){
-	uniqueOnions.eachParallel{onion ->
-	    testConnection(onion)
-            spider(onion)
-        }
+        String data = new URL(address).getText()
+        def uniqueOnions = data.findAll(/(?:([a-z]+):\/\/){0,1}([a-z2-7]{16})\.onion(?::(\d+)){0,1}/).unique()
+        if(uniqueOnions){
+           uniqueOnions.eachParallel{onion ->
+               testConnection(onion)
+               spider(onion)
+           }
+       }
     }
-} catch (java.io.IOException ex){
-    println "$address Failed - Can't Spider"
-}
+    catch (java.io.IOException ex){
+        println "$address Failed - Can't Spider"
+    }
 }
 
 
@@ -30,11 +29,12 @@ def testConnection(address){
     try {
         connection.connect()
         println "$address - ${connection.getResponseCode()}"
-    } catch(java.net.SocketException ex){
-        println "$address - No Connection"
-    }
+        }
+    catch(java.net.SocketException ex){
+            println "$address - No Connection"
+        }
 }
 
 withPool{
-spider('http://thehiddenwiki.org')
+    spider('http://thehiddenwiki.org')
 }
