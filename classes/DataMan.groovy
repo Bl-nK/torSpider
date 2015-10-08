6e6b9f5f552e7839c77c93627e622eb50a7d0afc
Brandon <brandon@bl-nk.net>
2015-10-08 14:59:41 -0500

    SQL modifications

--- classes/DataMan.groovy
+++ classes/DataMan.groovy
@@ -10,15 +10,23 @@ class DataMan{
 
 	def createDB(){
 	    sql.execute("drop table if exists onions")
-	    sql.execute("create table onions (id integer primary key, url string, parent integer references onions (id), spidered boolean, tested boolean)")
+	    sql.execute("create table onions (id integer primary key, url string, parent integer references onions (id), spidered boolean, tested boolean, status string)")
 
 	}
 
 	def addOnions(List onions, String parent){
 	    sql.execute("PRAGMA foreign_keys = ON")
-	    def parentId = sql.executeInsert("insert or ignore into onions (url,spidered,tested) values (?,?,?)",[parent,true,true])
+	    def parentId = sql.executeInsert("insert or ignore into onions (url,spidered,tested,status) values (?,?,?,?)",[parent,true,true,'success'])
 	    onions.each{
-		def insertStatement = sql.execute("insert into onions (url,parent,spidered,tested) values (?,?,?,?)",[it,parentId[0][0],false,false])
+		def insertStatement = sql.execute("insert into onions (url,parent,spidered,tested,status) values (?,?,?,?,?)",[it,parentId[0][0],false,false,null])
+	    }
+	}
+
+	def getOnionsForTesting(String operation){
+	    if (operation == "test"){
+	        def toTest = sql.rows('select url from onions where not (tested)')
+	    } else if (operation == "spider"){
+	        def toTest = sql.rows('select url from onions where not (spidered)')
 	    }
 	}
 
